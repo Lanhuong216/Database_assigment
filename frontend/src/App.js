1e5850610ee4c55d8900240c14092a1d6fe9ab24
unknown <vuhoan0095@gmail.com>
2024-12-06 13:58:32 +0700

    some pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
-import logo from './cohon.png';
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Header from './components/Header';
-import Overview from './components/Overview';
 import Orders from './pages/Orders';
 import Customers from './pages/Customers';
 import Marketing from './pages/Marketing';
@@ -11,13 +10,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Sidebar />
-      <div className="main-content">
-        <Header />
-        <Overview />
+    <Router>
+      <div className="App">
+        <Sidebar />
+        <div className="main-content">
+          <Header />
+          <Routes>
+            <Route path="/" element={<h1>Trang Tổng Quan</h1>} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/customers" element={<Customers />} />
+            <Route path="/marketing" element={<Marketing />} />
+            <Route path="/inventory" element={<Inventory />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
