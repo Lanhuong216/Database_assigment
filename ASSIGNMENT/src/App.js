0b52ca87b8b49572e95741e20b92e8188a1440a4
Lanhuong22 <huong.nguyenlancs22@hcmut.edu.vn>
2024-12-07 17:58:39 +0700

    add, info

--- ASSIGNMENT/src/App.js
+++ ASSIGNMENT/src/App.js
@@ -1,15 +1,24 @@
-import Navbar from './components/NavigationBar/Navbar'
 import {
   BrowserRouter as Router,
   Routes,
   Route
 } from "react-router-dom";
+import Navbar from './components/NavigationBar/Navbar'
+import CusInfo from "./pages/cusInfo";
+import EmpInfo from "./pages/empInfo";
+import AddNewCus from "./pages/addnewCus";
+import AddNewEmp from "./pages/addnewEmp";
 function App() {
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<Navbar />} />
+          <Route path='/listcus/:cusId' element={<CusInfo />} />
+          {/* /listcus/deactive element={<CusList/>*/}
+          <Route path='/listemp/:empId' element={<EmpInfo />} />
+          <Route path='/listcus/addcus' element={<AddNewCus />} />
+          <Route path='/listemp/addemp' element={<AddNewEmp />} />
         </Routes>
       </Router>
     </>
